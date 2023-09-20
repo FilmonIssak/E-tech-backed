32680f2589636d2ea1e881fb8f84f03c7372ed1a
Samsom Tesfabrhan <sasitesfa@gmail.com>
2023-09-19 22:40:23 -0400

    fixed some issues

--- src/main/java/com/Etech/Controller/AdminController.java
+++ src/main/java/com/Etech/Controller/AdminController.java
@@ -10,45 +10,45 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/admin/products")
+@RequestMapping("/api/v1/admin")
 public class AdminController {
 
     @Autowired
     private  AdminService adminService;
 
-    @PostMapping
+    @PostMapping("/products")
     public ResponseEntity<?> addProduct(@RequestBody ProductDto productDto) {
         return ResponseEntity.status(HttpStatus.OK).body(adminService.addProduct(productDto));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("products/{id}")
     public ResponseEntity<?> findProductById(@PathVariable("id") long id) {
         return ResponseEntity.status(HttpStatus.OK).body(adminService.findProductById(id));
     }
 
-    @GetMapping
+    @GetMapping("products")
     public ResponseEntity<List<?>> getAllProducts() {
        return ResponseEntity.status(HttpStatus.OK).body(adminService.findAllProduct());
     }
 
-    @PatchMapping("description/{id}")
+    @PatchMapping("products/description/{id}")
     public ResponseEntity<?> updateProductDescription(@PathVariable long id, @RequestBody ProductDto productDto) {
         return ResponseEntity.status(HttpStatus.OK).body(adminService.updateProductDescription(id,productDto));
     }
 
-    @PatchMapping("price/{id}")
+    @PatchMapping("products/price/{id}")
     public ResponseEntity<?> updatePrice(@PathVariable long id, @RequestBody ProductDto productDto) {
         return ResponseEntity.status(HttpStatus.OK).body(adminService.updateProductPrice(id, productDto));
     }
-    @PatchMapping("category/{id}")
+    @PatchMapping("products/category/{id}")
     public ResponseEntity<?> updateCatagory(@PathVariable long id, @RequestBody ProductDto productDto) {
         return ResponseEntity.status(HttpStatus.OK).body(adminService.updateProductCategory(id, productDto));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("products/{id}")
     public ResponseEntity<?> deleteProduct(@PathVariable long id) {
         adminService.deleteProduct(id);
-        return ResponseEntity.status(HttpStatus.OK).body("Product to be deleted not present");
+        return ResponseEntity.status(HttpStatus.OK).body("Product Successfully deleted");
     }
 
 
--- src/main/java/com/Etech/Controller/CustomerController.java
+++ src/main/java/com/Etech/Controller/CustomerController.java
@@ -1,24 +1,22 @@
 package com.Etech.Controller;
 
-import com.Etech.Model.enums.ProductCategory;
 import com.Etech.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/customer/products")
+@RequestMapping("/api/v1/customer")
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @GetMapping
+    @GetMapping("/products")
     public ResponseEntity<?> getAllProducts() {
         return ResponseEntity.status(HttpStatus.OK).body(customerService.findAll());
     }
